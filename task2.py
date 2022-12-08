5b1b0bf6e6433e50e1fddc65b135b24fdf71cb8a
Regina Shakirova <reginafar22@rambler.ru>
2022-12-08 16:00:26 +0400

    правки

--- task2.py
+++ task2.py
@@ -3,6 +3,6 @@
 # Пример исходного списка: [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55].
 # Результат: [12, 44, 4, 10, 78, 123].
 
-my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-new_list = [my_list[i] for i in range(1, len(my_list)) if my_list[i] > my_list[i-1]]
-print(new_list)
+list_1 = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+result = [list_1[i] for i in range(1, len(list_1)) if list_1[i] > list_1[i-1]]
+print(result)
